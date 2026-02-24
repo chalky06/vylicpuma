6085f07db7162a21d652e4060aebfe5b7d8b17e6
chalky06 <152178285+chalky06@users.noreply.github.com>
2026-02-24 21:25:07 +0000

    Update bg.js

--- js/bg.js
+++ js/bg.js
@@ -1,8 +1,3 @@
-// bg.js
 const textBg = document.querySelector('.text-bg');
-
-// Base string (replace with your full text)
 const baseString = "cffgfevqcfbtuuifgsjvmjhspvoesvncmfeboesbuttqsvohpvu"; 
-
-// Repeat enough times to fill the screen
 textBg.textContent = baseString.repeat(50); 
