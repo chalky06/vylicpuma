4c23e8c9ea3a49f62696f4f9fe73802f255e81b4
chalky06 <152178285+chalky06@users.noreply.github.com>
2026-02-24 17:24:28 +0000

    Add password validation to login form

--- js/crypto.js
+++ js/crypto.js
@@ -1,1 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
 
+  const form = document.getElementById("loginForm");
+  const message = document.getElementById("message");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const input = document.getElementById("password").value.trim();
+
+    if (input === "CAIUS") {
+      window.location.href = "puzzles/puzzle1.html";
+    } else {
+      message.textContent = "Access denied.";
+    }
+
+  });
+
+});
